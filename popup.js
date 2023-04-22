2900ebb14b02040896d22b669c27f0caba80e629
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-04-22 15:06:26 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".changeColorBtn");
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
+
+btn.addEventListener("click", async () => {
+  
+
+
+});
+
+
