da29fdcae81eb2ce47360fb2cfbd409398cd49b1
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-04-24 11:58:57 +0530

    -

--- popup.js
+++ popup.js
@@ -1,11 +1,50 @@
 const btn = document.querySelector(".changeColorBtn");
-const colorGrid = document.querySelector(".colorGrid");
-const colorValue = document.querySelector(".colorValue");
 
-btn.addEventListener("click", async () => {
-  
+let content = document.querySelector(".content");
+
+chrome.runtime.onMessage.addListener((request,sender,sendResponse) => {
+
+  let res = request.docTitleText;
+
+  console.log(res);
+
+  if(res != null){
+
+    content.innerHTML = res;
+
+  }
+
+  else{
+
+    alert("No data found");
+  }
 
 
 });
 
+btn.addEventListener("click", async () => {
 
+  let [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+
+  chrome.scripting.executeScript({
+
+    target: { tabId: tab.id },
+    function: scrapeJudgement
+
+  });
+});
+
+
+function scrapeJudgement(){
+
+  let judgement = document.querySelectorAll(".article");
+
+  let docTitleText = judgement[0].innerText;
+
+  // alert(docTitleText);
+
+  chrome.runtime.sendMessage({docTitleText});
+}
